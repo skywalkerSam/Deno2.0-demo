32b2ea6d0ffc37767955156752751e56c87bd7bf
Starboy <samskywalker001@gmail.com>
2025-01-03 15:25:14 +0000

    demo

--- Demo/hello.ts
+++ Demo/hello.ts
@@ -1,8 +1,8 @@
 function hello(params: string) {
-  console.log(`Hello, ${params}`);
+  console.log(`Hello, ${params}!`);
 }
 
 // const output = hello("World");
 // console.log(output);
 
-console.log(hello("and fuck you.)"));
+console.log(hello("DENO 2.0"));
